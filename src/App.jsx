cffd80816f5b6f28c86d9975e3e52e632b04c61f
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-04-30 14:33:35 -1000

    imported dependecies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Clock from './Clock';
+import React  from 'react';
+import Clock from 'react-live-clock';
 import './App.css';
 
 class App extends Component {
@@ -18,6 +20,15 @@ class App extends Component {
 
   render() {
     return (
+  <div>
+    <div className="clock-div">
+      <h1 className="nyText">New York</h1>
+      <div>
+      <h1 className="hiText">Hawaii</h1>
+        <clock timezone-offset="-10" ></clock>
+      </div>
+      <h1 className="tText">Tokyo</h1>
+    </div>
     <div className="App">
       <div className="App-title">Countdown to <div className="Date-title">{this.state.deadline}</div></div>
       <Clock
@@ -32,6 +43,7 @@ class App extends Component {
         <button className="button button1" onClick={() => this.changeDeadline()}>Submit</button>
       </div>
     </div>
+  </div>
     )
   }
 }
